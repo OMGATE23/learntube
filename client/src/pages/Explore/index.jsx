ddc0e943ca8352d224b6963bb5cc7e40e5546e62
omgate <90276220+OMGATE23@users.noreply.github.com>
2023-04-08 20:14:19 +0530

    style: pages UI

--- client/src/pages/Explore/index.jsx
+++ client/src/pages/Explore/index.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import Sidebar from '../../components/Sidebar'
+import data from '../../data/explorePlaylist.json'
 
 const Explore = () => {
   return (
-    <div>Explore</div>
+    <div className='flex'>
+        
+        <Sidebar/>
+        <div className=' mx-auto mb-20 w-[90%]'>
+            <div className=' flex  items-center justify-center h-[5rem]'>
+                <label className=' w-2/3 flex items-center gap-4 justify-center'>
+                    <span className='text-xl'>Search: </span>
+                    <input className='outline outline-1 px-4 text-gray-800 outline-gray-600 rounded-xl bg-gray-100 w-4/5 h-[2.5rem]' type='text' />
+                </label>
+            </div>
+        <div className='grid grid-cols-1 w-[80%] mx-auto md:grid-cols-3 gap-4 justify-items-center'>
+        {
+            data.map(playlist => {
+                console.log(playlist.playlist)
+                let {channelName , playlistId , thumbnails,  title , videoCount} = playlist.playlist
+
+                if(title.length > 30){
+                    title = title.substring(0 , 27) + '...' 
+                }
+
+                if(channelName.length > 20) {
+                    channelName = channelName.substring(0 , 17) + "..."
+                }
+                return(
+                    <div key = {playlistId} className=' transition-all duration-200  w-[300px] h-[300px] text-center rounded-xl shadow-xl p-6 hover:cursor-pointer hover:bg-gray-100'>
+                        <img src = {thumbnails[3].url} alt = {title}/>
+                        <p className='text-lg mt-4'>{title}</p>
+                        <p className='text-gray-700'>{channelName}</p>
+                        <p>No of videos : {videoCount}</p>
+                        
+                    </div>
+                )
+            })
+        }
+        </div>
+        </div>
+        
+        
+    </div>
   )
 }
 
--- client/src/pages/LandingPage/index.jsx
+++ client/src/pages/LandingPage/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Sidebar from '../../components/Sidebar'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div>
+        
+        <div className='h-[100vh] flex flex-col justify-center outline outline-2 bg-gray-50 text-center'>
+    
+        <h1 className='text-8xl font-bold'><span className='text-red-600'>YouTube</span> <span className='text-gray-900'>Courses</span></h1>
+        <p className='mt-2 text-gray-700 text-lg'>"Unlock the Power of YouTube Learning with Our Course-Boosting App!"</p>
+        <a href = '/signin' className=' transition-all duration-200 border-2 w-[10rem] py-2 mt-6 text-lg font-semibold border-gray-800 rounded-2xl mx-auto hover:text-white hover:bg-gray-800'>Sign In!</a>
+    </div>
+    </div>
+    
   )
 }
 
