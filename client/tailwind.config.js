c5e0245cdfc9d5522abd5d0086b20abcfbfbe0c3
omgate <90276220+OMGATE23@users.noreply.github.com>
2023-04-08 12:12:18 +0530

    setup : tailwind setup

---
+++ client/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1000px',
+      xl: '1440px',
+    },
+    extend: {
+      fontFamily : {
+        'poppins' : ['Poppins']
+      } ,
+      colors : {
+
+      }
+    },
+  },
+  plugins: [],
+}
+
