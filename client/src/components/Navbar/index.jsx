bd753e1d8354e68fb741d1b741905c9d59bd97b3
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2023-04-09 11:36:01 +0530

    style: signin page

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import styles from 'Navbar.module.css'
+import React from "react";
+
 const Navbar = () => {
   return (
-    <div>
-        
+    <div
+      className="flex items-center pl-10 py-2 border-b border-gray-600"
+    >
+      <img src="./logo.png" alt="" width="40" height="40" />
+      <p className="ml-2">
+        <span className="text-white">Youtube</span>
+        <span className="text-red-600">Learn</span>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
