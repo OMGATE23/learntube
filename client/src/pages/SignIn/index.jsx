afc2dcc96d881e6f8d167b2e304d02d92690e29e
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2023-04-09 11:36:15 +0530

    refactor: prettier

--- client/src/pages/SignIn/index.jsx
+++ client/src/pages/SignIn/index.jsx
@@ -33,7 +33,10 @@ const SignIn = () => {
 
       localStorage.setItem(
         "user",
-        JSON.stringify({ ...apiRes.data.user, avatarUrl: userBody.displayPicture })
+        JSON.stringify({
+          ...apiRes.data.user,
+          avatarUrl: userBody.displayPicture,
+        })
       );
 
       navigate("/dashboard");
@@ -53,38 +56,42 @@ const SignIn = () => {
 
   return (
     <>
-    <Navbar />
-    <div className=" h-[90vh] flex justify-center items-center text-white">
-      <div className="flex flex-col justify-center items-center shadow-xl rounded-2xl w-[400px] text-center gap-4 py-8 border border-gray-500 bg-base-100/10 bg-gradient-to-r from-transparent to-base-100/50">
-        
-        <img src="./logo.png" alt="" width="70" height="70" />
-        <div>
-          <h1 className="text-4xl font-semibold">Sign In</h1>
-          <p className="text-sm mt-2">To LearnTube</p>
+      <Navbar />
+      <div className=" h-[90vh] flex justify-center items-center text-white">
+        <div className="flex flex-col justify-center items-center shadow-xl rounded-2xl w-[400px] text-center gap-4 py-8 border border-gray-500 bg-base-100/10 bg-gradient-to-r from-transparent to-base-100/50">
+          <img src="./logo.png" alt="" width="70" height="70" />
+          <div>
+            <h1 className="text-4xl font-semibold">Sign In</h1>
+            <p className="text-sm mt-2">To LearnTube</p>
+          </div>
+
+          {!loading && (
+            <button
+              onClick={signinWithGoogle}
+              className=" block mt-4 transition-all duration-200 border-2 w-fit py-2 px-6 text-lg font-normal border-gray-800 rounded-md mx-auto hover:text-white hover:bg-blue-600 hover:border-blue-600"
+            >
+              <img
+                src="./googleicon.png"
+                width="20"
+                height="20"
+                className="inline-block mr-3"
+              />
+              Sign In with Google
+            </button>
+          )}
+          {loading && (
+            <button
+              disabled
+              className="block bg-gray-300 mt-4 transition-all duration-200 border-2 w-fit py-2 px-6 text-lg font-normal rounded-md mx-auto cursor-not-allowed"
+            >
+              <img src="./googleicon.png" width="20" height="20" />
+              Signing in ...
+            </button>
+          )}
+
+          {error && <p className="text-red-500 text-sm">{error}</p>}
         </div>
-
-        {!loading && (
-          <button
-            onClick={signinWithGoogle}
-            className=" block mt-4 transition-all duration-200 border-2 w-fit py-2 px-6 text-lg font-normal border-gray-800 rounded-md mx-auto hover:text-white hover:bg-blue-600 hover:border-blue-600"
-          >
-            <img src="./googleicon.png" width="20" height="20" className="inline-block mr-3" />
-            Sign In with Google
-          </button>
-        )}
-        {loading && (
-          <button
-            disabled
-            className="block bg-gray-300 mt-4 transition-all duration-200 border-2 w-fit py-2 px-6 text-lg font-normal rounded-md mx-auto cursor-not-allowed"
-          >
-            <img src="./googleicon.png" width="20" height="20"/>
-            Signing in ...
-          </button>
-        )}
-
-        {error && <p className="text-red-500 text-sm">{error}</p>}
       </div>
-    </div>
     </>
   );
 };
