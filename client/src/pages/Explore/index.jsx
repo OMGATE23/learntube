66d8c719ff7e194a9c7fbf16131fe1deb26336c3
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2023-04-09 12:25:42 +0530

    feat: explore page

--- client/src/pages/Explore/index.jsx
+++ client/src/pages/Explore/index.jsx
@@ -3,6 +3,7 @@ import Sidebar from "../../components/Sidebar";
 import data from "../../data/explorePlaylist.json";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import { useNavigate, Link } from "react-router-dom";
+import { MagnifyingGlassIcon   } from "@heroicons/react/24/outline";
 
 const Explore = () => {
   const { user } = useAuthContext();
@@ -17,13 +18,15 @@ const Explore = () => {
   return (
     <div className="flex">
       <Sidebar />
-      <div className=" mx-auto mb-20 w-[90%]">
-        <div className=" flex mr-12 items-center justify-end h-[5rem]">
+      <div className="mx-auto mb-20 w-[90%]">
+        <div className="flex items-center justify-between w-[80%] mx-auto my-5 px-2">
+          <h1 className="text-white font-semibold text-2xl">Explore</h1>
           <Link
             to="/search"
-            className="text-lg transition-all duration-200 border flex gap-1 border-gray-700 py-2 px-4 rounded-xl hover:bg-gray-200"
+            className="text-lg transition-all duration-200 border flex gap-1 border-gray-700 py-2 px-6 rounded-xl hover:bg-gray-200 text-white hover:text-black"
           >
-            <span className="hidden md:block">Go to Search</span>
+            <span className="hidden md:block mr-2">Search</span>
+            <MagnifyingGlassIcon className="w-[26px] lg:w-[20px]" width={20} />
           </Link>
         </div>
         <div className="grid grid-cols-1 w-[80%] mx-auto md:grid-cols-3 gap-4 justify-items-center">
@@ -42,12 +45,12 @@ const Explore = () => {
             return (
               <div
                 key={playlistId}
-                className=" transition-all duration-200  w-[300px] h-[300px] text-center rounded-xl shadow-xl p-6 hover:cursor-pointer hover:bg-gray-100"
+                className=" transition-all duration-200  w-[300px] h-[300px] text-center rounded-xl shadow-xl p-6 hover:cursor-pointer border-2 border-[rgba(30,224,224,.2)] bg-base-100/10 bg-gradient-to-r from-transparent to-base-100/50 "
               >
                 <img src={thumbnails[3].url} alt={title} />
-                <p className="text-lg mt-4">{title}</p>
-                <p className="text-gray-700">{channelName}</p>
-                <p>No of videos : {videoCount}</p>
+                <p className="text-lg mt-4 text-white">{title}</p>
+                <p className="text-gray-400">{channelName}</p>
+                <p className="text-gray-500">No of videos : {videoCount}</p>
               </div>
             );
           })}
